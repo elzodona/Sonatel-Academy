bdfe05fe2e8e34aaa0d987de9495bffae4a07ed5
CybeRooT-01 <61121866+CybeRooT-01@users.noreply.github.com>
2023-02-01 12:39:55 +0000

    controle les saisie

--- Langage C/Séquence/seq.c
+++ Langage C/Séquence/seq.c
@@ -4,14 +4,25 @@ int main() {
   int n, i, longueur_max = 0, longueur = 1;
   int val_precedent, val_actuel;
   int start = 0; //premier position de la sequence la plus longue
-  
-  // Saisie de N
-  printf("Entrez la valeur de N (entre 10 et 50): ");
-  scanf("%d", &n);
 
-  // Saisie du premier entier
-  printf("Entrez un entier (entre 1 et 100): ");
-  scanf("%d", &val_precedent);
+  // Saisie de N et controle si N est entre 10 et 50
+  do {
+    printf("Entrez la valeur de N (entre 10 et 50): ");
+    scanf("%d", &n);
+    if (n < 10 || n > 50) {
+      printf("La valeur de N doit etre entre 10 et 50. \n");
+    }
+  } while (n < 10 || n > 50);
+
+
+  // Saisie du premier entier et controle si il est entre 1 et 100
+  do {
+    printf("Entrez un entier (entre 1 et 100): ");
+    scanf("%d", &val_precedent);
+    if (val_precedent < 1 || val_precedent > 100) {
+      printf("La valeur doit etre entre 1 et 100. \n");
+    }
+  } while (val_precedent < 1 || val_precedent > 100);
 
   // Recherche de la plus longue séquence croissante
   for (i = 1; i < n; i++) {
