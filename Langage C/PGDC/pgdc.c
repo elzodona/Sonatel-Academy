a9304c679a492d0e3f092bbda73b96b5ae6cebeb
Doucour <124135808+Doucour@users.noreply.github.com>
2023-02-01 10:01:51 +0000

    Update pgdc.c

--- Langage C/PGDC/pgdc.c
+++ Langage C/PGDC/pgdc.c
@@ -1,12 +1,32 @@
+
 #include <stdio.h>
 int main(){
     int a;
     int b;
     int tmp;
-    printf("Donnez le premier nombre: ");
-    scanf("%d", &a);
-    printf("Donnez le deuxieme nombre: ");
-    scanf("%d", &b);
+    
+    do
+    {
+     printf("Donnez le premier nombre: \n");
+     scanf("%d", &a);
+    if (a<0)
+    {
+       printf("l'entier saisi doit etre positif\n");
+    }
+    
+    } while (a<=0);
+    
+    do
+    {
+         printf("Donnez le deuxieme nombre: \n");
+     scanf("%d", &b);
+    if (b<0)
+    {
+       printf("l'entier saisi doit etre positif\n");
+    }
+    } while (b<=0);
+    
+
     //s'assurer que a est le plus grand
     if(a<b){
         tmp=a;
