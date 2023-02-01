1ad6e0ea7ba6f8857610df0aec75d5bf018e0422
CybeRooT-01 <61121866+CybeRooT-01@users.noreply.github.com>
2023-02-01 14:31:53 +0000

    Update ppcm.c

--- Langage C/PPCM/ppcm.c
+++ Langage C/PPCM/ppcm.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 int main(){
- int a;
- int b;
- int tmp;
- printf("Donnez le valeur de a :");
- scanf("%d", &a);
-  printf("Donnez le valeur de a :");
- scanf("%d", &b);
- tmp = b;
- while (tmp % a != 0)
- {
+int a;
+int b;
+int tmp;
+
+printf("Donnez le valeur de a :");
+scanf("%d", &a);
+printf("Donnez le valeur de a :");
+scanf("%d", &b);
+tmp = b;
+
+while (tmp % a != 0)
+{
     tmp+=b;
- }
- printf("Le PPCM est de : %d", tmp);
+}
+printf("Le PPCM est de : %d", tmp);
  
 }
